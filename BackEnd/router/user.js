13fe3bc02e833d841346bca49e9c193122dec1ed
Md Kashif Zafar <110238257+kashi159@users.noreply.github.com>
2023-03-22 16:01:33 +0530

    Task 5 Commit Make the Login backend api

--- BackEnd/router/user.js
+++ BackEnd/router/user.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const userController = require('../controller/user');
 
 router.post('/signup', userController.postSignUpUser);
+router.post('/login', userController.postLoginUser)
 
 module.exports = router;
