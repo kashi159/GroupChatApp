31a10786c05b3d72e7ec2b810f069839bc52c353
Md Kashif Zafar <110238257+kashi159@users.noreply.github.com>
2023-04-02 02:50:22 +0530

    .

--- models/files.js
+++ models/files.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const sequelize = require('../util/userDatabase');
+const sequelize = require('../util/database');
 
 const File = sequelize.define('filelinks', {
     id: {
