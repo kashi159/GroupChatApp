5a1632a542fae04c212467be9450d396a2ce0185
Md Kashif Zafar <110238257+kashi159@users.noreply.github.com>
2023-04-02 03:02:54 +0530

    .

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ io.on('connection', (socket) => {
         const s3Result = await s3.upload(s3Params).promise();
         // console.log(s3Result);
         const link = await File.create({
-          fileUrl: s3Result.Location,
+          fileURL: s3Result.Location,
           userId: data.userId,
           groupId: data.groupId
         })
