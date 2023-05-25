343a9731dc7698c5dd2a7085315f95e681094c93
Md Kashif Zafar <110238257+kashi159@users.noreply.github.com>
2023-05-25 18:11:22 +0530

    ..

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ const s3 = new AWS.S3();
 
 const io = require('socket.io')(4000,{
     cors:{
-        origin: ["https://group-chatwithfriends.000webhostapp.com","https://admin.socket.io"]
+        origin: ["http://127.0.0.1:5500/","https://admin.socket.io"]
     }
 })
 
